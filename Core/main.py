22dbc9b1fec8d22e7aa852e1eee1514b162ddefb
mishiki1002 <brownbear1002@gmail.com>
2017-04-21 14:13:02 -0400

    Added dependencies to Root directory as well as eddited the init script

--- Core/main.py
+++ Core/main.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
-# Remember to install aiml using *** pip install aiml
 import aiml
 import os
 
 # Aiml
 kury = aiml.Kernel()
-sessionId = 00001
-
-# Loading Files
-kury.learn("Services/Voice/Chatbot/bot.aiml")
 
 running = True
 
 class main():
-  Version = '0.0.1'
-  Author = 'Josh Brown'
-  Email = 'Brownbear1002@gmail.com'
   
-  def init():
+  def init(self):
     #Initalize voice
     print("Main: Stating Services")
 
     print("Main: Starting Voice")
-
+      
+    kury.learn("Services/Voice/Chatbot/bot.aiml")
     print("Main: Starting Web Gui")
-
+    #os.system("cd Services/Server && python -m SimpleHTTPServer 8000")
     print("Main: Starting AIML")
     
 
+core = main()
+
+core.init()
+
 while running == True:
   message =  kury.respond(raw_input("User >> "))
-  print("Chatbot >> " + kury.respond(message))
+  print("Kury >> " + kury.respond(message))
