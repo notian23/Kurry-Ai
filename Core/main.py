0a310736937c3d375da26267e670d7be7fd0356d
Josh Brown <brownbear1002@gmail.com>
2017-04-20 20:49:18 -0400

    Added some ai features as well as implementing that with main.py

--- Core/main.py
+++ Core/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Remember to install aiml using *** pip install aiml
+import aiml
+import os
 
 # Debugging Purposes
 from Gestures import Hand
@@ -14,10 +17,50 @@ from Gestures import Head
 # arm = Arm()
 # head = Head()
 
+# Aiml
+kurry = aiml.Kernel()
+sessionId = 00001
+
+# Speeding up load times
+if os.path.isfile("Services/Voice/Chatbot/bot_brain.brn"):
+  kurry.bootstrap(brainFile = "Services/Voice/Chatbot/bot_brain.brn")
+else:
+  kurry.bootstrap(learnFiles = "Services/Voice/Chatbot/Startup.aiml", commands = "load aiml")
+  kurry.saveBrain("Services/Voice/Chatbot/bot_brain.brn")
+
 class main():
   Version = '0.0.1'
   Author = 'Josh Brown'
   Email = 'Brownbear1002@gmail.com'
   
   def init():
-    # hand.init()
+    Hand_Max = hand.options.max()
+    Arm_Max = arm.options.max()
+    Head_Max = head.options.max()
+    
+    Hand_Current = 0
+    Arm_Current = 0
+    Head_Current = 0
+    
+    #Save info to varialbe
+    User_Data = input("Your Input: ")
+    
+    if(User_Data == hand.options(0)):
+      print("Hello World")
+    
+    else (Hand_Current > Hand_Max):
+      Hand_Current++
+    
+    else (Hand_Current = Hand_Max):
+      Hand_Current = 0
+      
+#Must be last thing to run
+while True:
+  message =  kurry.respond(raw_input("Kurry >> "))
+  
+  if message == "quit" or "Save":
+    exit()
+  else if message == "save" or "Save":
+    kernel.saveBrain("Services/Voice/Chatbot/bot_brain.brn")
+  else:
+    bot_response = kurry.respond(message)
